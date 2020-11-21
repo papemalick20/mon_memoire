607435b6437b6c6e7f41dd9dbc3dd468bfb20d3d
Thioune malick <emorocoly20@gmail.com>
2020-11-21 21:55:06 +0000

    first envoie

---
+++ src/Controller/HomController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class HomController extends AbstractController
+{
+     public function __construct(Environment $twig)
+     {
+         $this->twig=$twig;
+     }
+    /**
+     * @Route("/hom", name="hom")
+     */
+    public function home(): Response
+    {
+        return $this->render('hom/index.html.twig');
+    }
+}
